7ec5cd12618fed5da10f1fff73a943dd5d35834c
Fikky Miftakhul Huda <vqfikky93@gmail.com>
2021-08-12 08:04:24 +0700

    second

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+	public function profil()
+	{
+		$this->load->view('profil');
+	}
+	public function katalog()
+	{
+		$this->load->view('katalog');
+	}
+
+}
